3024380e6aecb7bec69d13506680fb32601a4034
Berrysoft <Strawberry_Str@hotmail.com>
2021-08-11 16:30:31 +0800

    Add drawing template.

--- Makefile
+++ Makefile
@@ -1,12 +1,25 @@
 .PHONY: all
 
-all: figures.pdf test.pdf
+all: figures.pdf
+
+.PHONY: test
+
+test: figures.pdf test.pdf
+
+.PHONY: clean
+
+clean:
+	rm -f data.h5
+	rm -f figures.pdf
+	rm -f test.pdf
 
 data.h5: geo.h5
 	python3 simulate.py -n 4000 -g $^ -o $@
 
-figures.pdf: data.h5
-	python3 draw.py $^ -o $@
+figures.pdf: data.h5 geo.h5
+	python3 draw.py $< -g $(word 2,$^) -o $@
 
 test.pdf: data.h5
 	python3 test.py $^ -o $@
+
+.SECONDARY:
--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import argparse
 
+# 处理命令行
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", dest="n", type=int, help="Number of events")
 parser.add_argument("-g", "--geo", dest="geo", type=str, help="Geometry file")
@@ -8,10 +9,14 @@ args = parser.parse_args()
 
 import h5py as h5
 
+# 读入几何文件
 with h5.File(args.geo, "r") as geo:
     print("TODO: Deal with geometry file")
 
+# 输出
 with h5.File(args.opt, "w") as opt:
+    # 循环模拟
     for i in range(args.n):
         print("TODO: Event", i)
+
     print("TODO: Write opt file")
